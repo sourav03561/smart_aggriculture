4a99162e5d090f406936b361ac2c87a0199271c5
Sourav Sarkar <46227372+sourav03561@users.noreply.github.com>
2023-11-09 21:19:01 +0530

    this file get data for visualization

---
+++ getData.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json');
+
+require_once('db.php');
+
+$sqlQuery = "SELECT N,P,K,temperature,humidity,ph,rainfall FROM data";
+$con = mysqli_connect("localhost:3306","root","Naren@03561","smart_aggriculture");
+$result = mysqli_query($con,$sqlQuery);
+
+$data = array();
+foreach ($result as $row) {
+	$data[] = $row;
+}
+
+
+
+echo json_encode($data);
+mysqli_close($con);
+?>
